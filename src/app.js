acdf2df3503008ebc1239262ca6224f4b5727c22
John Glenn Eligio <glenneligio@gmail.com>
2022-06-27 14:24:12 +0800

    Added WebSocket server and client

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const path = require("path");
+const socketIO = require("socket.io");
+const http = require("http");
+
 const app = express();
 const publicPath = path.join(__dirname, "/../public");
 
+// setup the Express server
 app.use(express.json());
 app.use(express.static(publicPath));
 
-app.get("/chat", (req, res) => {
-  res.send("Chat app");
+// create a Server instance using http.createServer() with Express Application
+const server = http.createServer(app);
+// add WebSocket support to the Server instance
+const io = new socketIO.Server(server);
+
+// define eventListener handlers of Server and sockets
+io.on("connection", (socket) => {
+  console.log("A user connection");
+  socket.on("disconnect", () => {
+    console.log("The user was disconnected");
+  });
 });
 
-module.exports = app;
+module.exports = server;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+// import necessary dependencies
 const express = require("express");
 const path = require("path");
-const app = express();
+const socketIO = require("socket.io");
+const http = require("http");
 
-const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT;
+const app = express();
+const publicPath = path.join(__dirname, "/../public");
 
+// setup the Express server
 app.use(express.json());
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, () => {
-  console.log("Server listening to port 3000");
+// create a Server instance using http.createServer() with Express Application
+const server = http.createServer(app);
+// add WebSocket support to the Server instance
+const io = new socketIO.Server(server);
+
+// define eventListener handlers of Server and sockets
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("The user was disconnected");
+  });
+});
+
+// make the Server instance listen to specific port
+server.listen(port, () => {
+  console.log("Server listening to " + port);
 });
